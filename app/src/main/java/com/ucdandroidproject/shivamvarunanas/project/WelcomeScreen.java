560df9a85c9b7a273f75877cc35ee37b9ce728a4
Shivam Rathore <r.shivam1993@gmail.com>
2018-11-30 17:35:07 +0000

    Style

--- app/src/main/java/com/ucdandroidproject/shivamvarunanas/project/WelcomeScreen.java
+++ app/src/main/java/com/ucdandroidproject/shivamvarunanas/project/WelcomeScreen.java
@@ -1,9 +1,9 @@
 package com.ucdandroidproject.shivamvarunanas.project;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ProgressBar;
 
 import com.felipecsl.gifimageview.library.GifImageView;
@@ -13,10 +13,16 @@ import org.apache.commons.io.IOUtils;
 import java.io.IOException;
 import java.io.InputStream;
 
+/*CREATED BY SHIVAM RATHORE
+WELCOME SCREEN IMPLEMENTATION*/
+
+
 public class WelcomeScreen extends AppCompatActivity {
+
 
     private GifImageView imageView;
     private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,14 +32,20 @@ public class WelcomeScreen extends AppCompatActivity {
         progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(progressBar.VISIBLE);
 
-        try{
+        //   final Typewriter writer = new Typewriter(this);
+        try {
             InputStream inputStream = getAssets().open("batmanrun.gif");
             byte[] in = IOUtils.toByteArray(inputStream);
             imageView.setBytes(in);
             imageView.startAnimation();
-        }catch (IOException e){
+        } catch (IOException e) {
 
         }
+//        writer.setCharacterDelay(150);
+//        writer.setTextSize(40);
+//         setContentView(writer);
+//        writer.animateText("FITNESS GURU");
+
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -42,6 +54,42 @@ public class WelcomeScreen extends AppCompatActivity {
                 ));
                 WelcomeScreen.this.finish();
             }
-        },4000);
+        }, 3000);
     }
 }
+//class Typewriter extends android.support.v7.widget.AppCompatTextView {
+//
+//    private CharSequence mText;
+//    private int mIndex;
+//    private long mDelay = 500; //Default 500ms delay
+//    private Handler mHandler = new Handler();
+//    private Runnable characterAdder = new Runnable() {
+//        @Override
+//        public void run() {
+//            setText(mText.subSequence(0, mIndex++));
+//            if (mIndex <= mText.length()) {
+//                mHandler.postDelayed(characterAdder, mDelay);
+//            }
+//        }
+//    };
+//
+//    public Typewriter(Context context) {
+//        super(context);
+//    }
+//    public Typewriter(Context context, AttributeSet attrs) {
+//        super(context, attrs);
+//    }
+//
+//    public void animateText(CharSequence text) {
+//        mText = text;
+//        mIndex = 0;
+//
+//        setText("");
+//        mHandler.removeCallbacks(characterAdder);
+//        mHandler.postDelayed(characterAdder, mDelay);
+//    }
+//
+//    public void setCharacterDelay(long millis) {
+//        mDelay = millis;
+//    }
+//}
